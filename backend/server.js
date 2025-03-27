8543c4940b5a0690677d5ebb870c8429b6b0c86d
Vishal Sharma <88317433+vishutrickcoder@users.noreply.github.com>
2025-03-27 12:19:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import { connectDb } from './config/db.js'
 import foodRouter from './routes/foodRoute.js'
-import path = from 'path';
+import path from 'path';
 
 import dotenv from "dotenv";
 import userRouter from './routes/userRoute.js';
